7403044aea62fd8d5c0c73bd4fe2f139bfcd325d
nithish-kumar-t <t.nithish136@gmail.com>
2026-02-20 23:47:27 -0600

    removed comments

--- app/main.go
+++ app/main.go
@@ -8,15 +8,8 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// TODO: Uncomment the code below to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
